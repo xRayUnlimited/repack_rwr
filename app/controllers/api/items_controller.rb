464cdb754a317a58e9a8c08a0f5364db82debeb4
Ray Suyoto <ray.suy@icloud.com>
2018-03-08 10:01:06 -0700

    update controller

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,2 +1,35 @@
 class Api::ItemsController < ApplicationController
+    before_action :set_tem, only: [:update, :destroy]
+    def index
+        render json: Item.all
+    end
+
+    def create
+        item = Item.new(item_params)
+
+        if item.save 
+            render json: item
+        else
+            render json: { errors: item.errors }, status: 422
+        end
+    end
+
+    def update
+        @item.update(complete: !@item.complete)
+        render json: @item
+    end
+
+    def destroy
+        @item.destroy
+    end
+
+
+    private
+    def item_params
+        prams.require(:item).permit(:name, :complete)
+    end
+
+    def set_item
+        @item = Item.find(params[:id])
+    end
 end
